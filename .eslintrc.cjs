fe730fe5ac6fbbf68c556314b6bca02d30dd2b9b
Valerie Ivashkevich <ivashkevich03@gmail.com>
2024-10-11 19:25:30 +0300

    chore: set up project configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: { node: true, es2022: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', 'coverage', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/extensions': ['.ts', 'tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', 'tsx'],
+    },
+  },
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+  },
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import 'dotenv/config';
+
+const sum = (a: number, b: number) => a + b;
+console.log(sum(1, 5));
+console.log(process.argv.slice(2));
